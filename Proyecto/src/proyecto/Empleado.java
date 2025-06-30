91dd344ef5a8fd411db9fa6920523ddb9ef32128
eliasgonzalez2V <elias.gonzalez@alu.inspt.utn.edu.ar>
2025-06-29 23:30:37 -0300

    Delete Proyecto directory

--- Proyecto/src/proyecto/Empleado.java
+++
@@ -1,8 +0,0 @@
-package proyecto;
-
-public class Empleado extends Persona {
-    public Empleado(String DNI, String nombre, String direccion, double telefono, String contrasenia) {
-        super(DNI, nombre, direccion, telefono, contrasenia);
-    }
-}
-
